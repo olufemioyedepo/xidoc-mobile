1473822f3ce1f11560e389ee2e04c1ffc78aa244
Olufemi Oyedepo <OOyedepo@wragbysolutions.com>
2020-01-24 14:05:50 +0100

    corrected a few typos

--- lib/src/helpers/variables.dart
+++ lib/src/helpers/variables.dart
@@ -6,9 +6,9 @@ const String baseUrl ="https://geofencingwebapi20191030062137.azurewebsites.net/
 const String ngrokBaseUrl ="http://68490929.ngrok.io/api/";
 const String currentFont = 'Montserrat';
 const String currencySymbol = '₦';
-const int customersPagePage = 10;
-const int salesOrdersPagePage = 10;
-const int paymentDepositsPagePage = 10;
+const int customersPerPage = 10;
+const int salesOrdersPerPage = 10;
+const int paymentDepositsPerPage = 10;
 
 
 const menuActions = <String>[
